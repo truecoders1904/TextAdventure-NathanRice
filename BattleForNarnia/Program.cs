7f2e3de88266525c74de628b322c53781f09bec8
Nathan <rocky@askoutthere.com>
2019-04-11 11:58:41 -0500

    further changes

--- BattleForNarnia/Program.cs
+++ BattleForNarnia/Program.cs
@@ -7,13 +7,18 @@ namespace BattleForNarnia
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, new recruit! Welcome to Narnia! My Name is Reepicheep!");
-            Console.WriteLine("I understand this is your first time on the Battlefield?");
+
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine()
+
+            Console.WriteLine($"Great to meet you {name}, I understand this is your first time on the Battlefield?");
 
             string response = Console.ReadLine();
 
                 if (response == "yes")
                 {
-                    Console.WriteLine("Not to worry! Let's get you fitted with Armour and a Blade!");
+                    Console.WriteLine($"Not to worry {name}! Let's get you fitted with Armour and a Blade!");
                 }
                 else if (response == "no")
                 {
@@ -21,6 +26,8 @@ namespace BattleForNarnia
                 }
             
             Console.WriteLine("So, Here we have our Blacksmith, Bandor.");
+
+            Console.WriteLine($"Bandor, this is {name}.);
 
             Console.WriteLine("Bandor: Lets get you fitted with armour first!");
 
@@ -54,12 +61,16 @@ namespace BattleForNarnia
                 {
                     Console.WriteLine("Great scott!! Your as big as our boulders! This Armour will allow you to be shot from our Catapults, safely of course!");
                 }
+            
+            Console.WriteLine("Now that your fitted, lets get you a blade!");
 
-                Console.WriteLine("Reepicheep: Now that your fitted, lets get you a blade!");
-                Console.WriteLine("This is is Rabidash, finest Bladesmith in all the land!");
-                Console.WriteLine("Rabidash: Hello there!");
+            Console.WriteLine($"{name}, this is is Rabidash, finest Bladesmith in all the land!");
 
-                 
+            Console.WriteLine($"Rabidash: Hello {name}!");
+
+            Console.WriteLine(");
+
+
         }
     }
 }
